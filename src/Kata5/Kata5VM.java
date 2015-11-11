3003308b4878664d4840ca48d09bfa8ef8284648
Khondax <Khondax@Khondax-PC>
2015-11-11 10:08:05 +0000

    Conexión con la base de datos. Mails tranferidos del archivo a la base de datos.

--- src/Kata5/Kata5VM.java
+++ src/Kata5/Kata5VM.java
@@ -1,5 +1,10 @@
 package Kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,7 +16,7 @@ import java.sql.Statement;
  * @author Khondax
  */
 public class Kata5VM {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
         
         Class.forName("org.sqlite.JDBC");
         Connection conec = DriverManager.getConnection("jdbc:sqlite:Kata5");
@@ -24,6 +29,20 @@ public class Kata5VM {
             System.out.println("ID = " + rs.getInt("ID"));
             System.out.println("NAME = " + rs.getString("NAME") + "\n");
         }
+        
+        
+        String nameFile = "E:\\Mis documentos\\Miguel\\Universidad\\Asignaturas\\IS2\\DATA\\emaildata.txt";
+        BufferedReader reader = new BufferedReader (new FileReader (new File (nameFile)));
+        String mail;
+        
+        while ((mail = reader.readLine())!= null){
+            String query = "INSERT INTO Mail (MAIL) VALUES ('" + mail + "')";
+            stat.executeUpdate(query);
+        }
+        
+        rs.close();
+        stat.close();
+        conec.close();
         
     }
 }
