6a65e938f0f96729c7f64feb0482682707155a77
Khondax <Khondax@10.225.172.150>
2015-11-25 09:43:57 +0000

    1. Inicio 2. Attribute, Person + modificaciones en MailListReader y controlador (kata6) 3. Modificaciones en HistogramDisplay y creamos HistogramBuilder (alternativa a la exsistente) 4. Modificaciones finales en Kata6, mostrar ambos histogramas.

--- src/Kata6/Kata6.java
+++ src/Kata6/Kata6.java
@@ -19,11 +19,32 @@ public class Kata6 {
         
         String nameFile = "E:\\Mis documentos\\Miguel\\Universidad\\Asignaturas\\IS2\\DATA\\emaildata.txt";
         
-        ArrayList<Person> mailArray = MailListReader.reader(nameFile);
+        ArrayList<Person> arrayMail = MailListReader.reader(nameFile);
+        
+        HistogramBuilder <Person> builder = new HistogramBuilder(arrayMail);
+        
+        Histogram <String> domains = builder.build(new Attribute<Person, String>() {
+
+            @Override
+            public String get(Person item) {
+                return item.getMail().split("@")[1];
+            }
+        });
+        
+        Histogram <Character> letters = builder.build(new Attribute<Person, Character>() {
+
+            @Override
+            public Character get(Person item) {
+                return item.getMail().charAt(0);
+            }
+        });
         
         //Histogram<String> histo = MailHistogramBuilder.build(mailArray);
         
         //new HistogramDisplay(histo).execute();
+        
+        new HistogramDisplay(domains, "DOMINIOS").execute();
+        new HistogramDisplay(letters, "CARACTER").execute();
         
         System.out.println("Fin");
     }
