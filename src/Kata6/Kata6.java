9be34c4cddac7edb6bb022cce236796afb2189bd
Khondax <Khondax@10.225.172.150>
2015-11-25 08:49:04 +0000

    1. Inicio

---
+++ src/Kata6/Kata6.java
@@ -0,0 +1,31 @@
+package Kata6;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Kata6 {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        System.out.println("Inicio");
+        
+        String nameFile = "E:\\Mis documentos\\Miguel\\Universidad\\Asignaturas\\IS2\\DATA\\emaildata.txt";
+        
+        ArrayList<String> mailArray = MailListReader.reader(nameFile);
+        
+        Histogram<String> histo = MailHistogramBuilder.build(mailArray);
+        
+        new HistogramDisplay(histo).execute();
+        
+        System.out.println("Fin");
+    }
+
+}
